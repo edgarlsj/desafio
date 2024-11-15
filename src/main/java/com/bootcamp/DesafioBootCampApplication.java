a73dcb2454e224244eb4452c8509139d70ef3552
Edgar Junior <edgarlsj@gmail.com>
2024-11-14 23:28:38 -0400

    alteração teste

--- src/main/java/com/bootcamp/DesafioBootCampApplication.java
+++ src/main/java/com/bootcamp/DesafioBootCampApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DesafioBootCampApplication {
 
     public static void main(String[] args) {
+        System.out.println();
         SpringApplication.run(DesafioBootCampApplication.class, args);
     }
 
